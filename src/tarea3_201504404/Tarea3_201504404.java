2d31d7f1ecb43a57a728a09f1863d78514741539
Axel <Axel@192.168.1.73>
2018-08-15 15:52:29 -0500

    estoy trabajando en mi tarea

---
+++ src/tarea3_201504404/Tarea3_201504404.java
@@ -0,0 +1,38 @@
+
+package tarea3_201504404;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Axel
+ */
+public class Tarea3_201504404 {
+
+    public static void main(String[] args) {
+       
+       
+        Scanner entrada= new Scanner(System.in);
+        int menu;
+        System.out.println("-----MENU-----");
+        System.out.println("1.- Usuarios");
+        System.out.println("2.- Contador de Digitos Repetidos");
+        System.out.println("3.- Tres Numeros de mayor a menor");
+        System.out.println("4.- Calcular Promedio");
+        System.out.println("5.- Salir");
+        System.out.println("Seleccione una opción");
+        menu=entrada.nextInt();
+        
+        Usuarios usu=new Usuarios();
+        
+       switch(menu){
+       
+        //llamar a la clase Usuarios   
+       case 1: 
+           usu.usuario();
+           System.out.println("");
+       break;         
+       
+    }
+    }
+}
---
+++ src/tarea3_201504404/Usuarios.java
@@ -0,0 +1,28 @@
+
+package tarea3_201504404;
+
+import java.util.Scanner;
+
+public class Usuarios {
+   
+    public static void main(String[] args) {
+   
+    }
+  public static  int usuario (){
+    
+    
+        Scanner entrada= new Scanner(System.in);
+
+        int menu;
+        System.out.println("-----USUARIO-----");
+        System.out.println("1.- Ingresar usuarios");
+        System.out.println("2.- Mostrar Usuarios Ascendente");
+        System.out.println("3.- Mostrar Usuarios Descendente");
+        System.out.println("4.- Menu Principal");
+       
+        menu=entrada.nextInt();
+        return 0;
+    
+  }
+
+}
