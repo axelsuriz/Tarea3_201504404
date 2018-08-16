8c10a1d93e2c1769f58f4d5092c29b533745aeb1
Axel <>
2018-08-16 00:53:35 -0500

    Trabajando en mi tarea(2)

--- src/tarea3_201504404/Usuarios.java
+++ src/tarea3_201504404/Usuarios.java
@@ -1,28 +1,59 @@
 
 package tarea3_201504404;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Usuarios {
-   
-    public static void main(String[] args) {
-   
-    }
-  public static  int usuario (){
-    
-    
-        Scanner entrada= new Scanner(System.in);
 
-        int menu;
+public class Usuarios {
+   public static void main(String[] args) {
+   
+  }
+       
+  public static Scanner entrada= new Scanner(System.in);
+  public static int menu; 
+  
+  public static  void usuario (){
+    
+    
+        
+
+        
         System.out.println("-----USUARIO-----");
         System.out.println("1.- Ingresar usuarios");
         System.out.println("2.- Mostrar Usuarios Ascendente");
         System.out.println("3.- Mostrar Usuarios Descendente");
         System.out.println("4.- Menu Principal");
-       
         menu=entrada.nextInt();
-        return 0;
+        
+        Usuarios usu=new Usuarios();
+        
+       switch(menu){
+           
+           case 1:
+               Usuarios.usua();
+               
+       }
+     
+       }
     
-  }
+       
+       public static String usua (){
+        
+         String usuarios []= new String[5];
+         String nombres; 
+            
+         for ( int i = 0; i <usuarios.length; i++) {
+          System.out.println("Usuario:  "+i);
+          nombres=entrada.nextLine();
+          usuarios[i]=nombres;
+          
+            
+         }
+       return null;
+      
+      
+    
+       }
 
-}
+    }
